8a0f0b7c1970db2130f578878cbef87b3fc2e036
Bruno Oliveira <bruno@abstractj.org>
2013-12-10 13:34:35 -0200

    Initial attempt to pipelines

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-define(['jquery', 'underscore'], function($, _, aerogear, text){
+define(['jquery', 'underscore', 'aerogear'], function($, _, aerogear){
   $(function(){
 
       var info = _.template( $("#info-template").text() );
@@ -7,6 +7,23 @@ define(['jquery', 'underscore'], function($, _, aerogear, text){
           var html = info( {'email': $("#email").val()} );
           $("#reset-page").html( html );
       });
-    });
+
+      var forgotPipeline = AeroGear.Pipeline([
+          {
+              name: "forgot",
+              settings: {
+                  baseURL: "http://localhost:8080/password-reset/rest/"
+              }
+          }
+      ]).pipes.forgot;
+
+      forgotPipeline.read({email : 'bruno@abstractj.com'} ,{
+          success: function( data ) {
+            console.log("Yay: " + data);
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+              console.log("WAT? " + textStatus);
+          }
+      });
   });
 });
